29382c29a6837e99dd7b21ef77687fa1c9cf7405
ChallengerCY <790597004@qq.com>
2020-02-26 00:04:49 +0800

    整理vector3 整理vector3 sqrMagnitude 官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3sqrMagnitudeOfficialDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3SqrMagnitudeOfficialDemo : MonoBehaviour
+    {
+        public Transform other;
+        public float closeDistance = 5.0f;
+
+        private void Start()
+        {
+            Debug.LogError("比较两个物体是否接近，推荐使用sqrMagnitude，向量的平方长度，性能比较好");
+        }
+
+        void Update()
+        {
+            if (other)
+            {
+                Vector3 offset = other.position - transform.position;
+                float sqrLen = offset.sqrMagnitude;
+
+                // square the distance we compare with
+                if (sqrLen < closeDistance * closeDistance)
+                {
+                    Debug.Log("The other transform is close to me!");
+                }
+            }
+        }
+    }
+}
+
+
