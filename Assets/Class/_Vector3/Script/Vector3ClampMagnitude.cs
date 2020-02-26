8eb02a2947dfe6a54d6a4e60ed7567e2fc838a75
ChallengerCY <790597004@qq.com>
2020-02-26 23:16:27 +0800

    查看vector3 clampmagnitude 和 cross 官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3ClampMagnitude.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3ClampMagnitude : MonoBehaviour
+    {
+        public Vector3 centerPt;
+        public float radius;
+
+        private void Start()
+        {
+            Debug.LogError("以centerPt为圆心，半径为radius 的范围内移动物体");
+        }
+
+        void Update()
+        {
+            // Get the new position for the object.
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+           
+            Vector3 newPos = transform.position + movement;
+
+            // Calculate the distance of the new position from the center point. Keep the direction
+            // the same but clamp the length to the specified radius.
+            Vector3 offset = newPos - centerPt;
+            //以centerPt为圆心，半径为radius 的范围内移动物体
+            transform.position = centerPt + Vector3.ClampMagnitude(offset, radius);
+        }
+    }
+}
---
+++ Assets/Class/_Vector3/Script/Vector3Cross.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3Cross : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+           Debug.LogError(@"求两个向量的叉积,两个向量的叉积生成第三个向量，
+           该向量垂直于两个输入向量。结果的大小等于：
+           将两个输入的大小相乘，然后乘以输入之间角度的正弦值。
+           可以使用“左手规则”确定结果向量的方向。");
+           
+           Debug.Log(GetNormal(Vector3.zero,Vector3.right,Vector3.up));
+        }
+        
+        /// <summary>
+        /// 获取叉积
+        /// </summary>
+        /// <param name="a">中间向量</param>
+        /// <param name="b">目标向量b</param>
+        /// <param name="c">目标向量c</param>
+        /// <returns></returns>
+        Vector3 GetNormal(Vector3 a, Vector3 b, Vector3 c)
+        {
+            // Find vectors corresponding to two of the sides of the triangle.
+            Vector3 side1 = b - a;
+            Vector3 side2 = c - a;
+
+            // Cross the vectors to get a perpendicular vector, then normalize it.
+            return Vector3.Cross(side1, side2).normalized;
+        }
+    }
+}
+
+
