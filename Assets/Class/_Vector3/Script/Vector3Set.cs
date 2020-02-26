5e7ba45390e2ba273315609eecb83609361c7ae5
ChallengerCY <790597004@qq.com>
2020-02-26 22:38:18 +0800

    查看vector3vector3 Set官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3Set.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Set : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Use this to set the new position of the GameObject
+    Vector3 m_MyPosition;
+
+    // Set an external Transform in the Inspector which is the GameObject’s starting point
+    public Transform m_NewTransform;
+
+    void Start()
+    {
+        // Set the new Vector to be that of the Transform you attach in the Inspector
+        m_MyPosition.Set(m_NewTransform.position.x, m_NewTransform.position.y, 0);
+    }
+
+    void OnGUI()
+    {
+        // Press the Button to set the GameObject to this new position
+        if (GUI.Button(new Rect(0, 0, 100, 40), "Set"))
+        {
+            transform.position = m_MyPosition;
+        }
+    }
+}
