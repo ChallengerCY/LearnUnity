124a6c11c60b0b4d38df1eb361250226a1aeeff6
ChallengerCY <790597004@qq.com>
2020-02-26 23:22:14 +0800

    查看vector3 distance 官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3Distance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3Distance : MonoBehaviour
+    {
+        
+       // Returns the distance between a and b.
+       // Vector3.Distance(a,b) is the same as (a-b).magnitude.
+        
+        // Start is called before the first frame update
+        public Transform other;
+
+        void Update()
+        {
+            if (other)
+            {
+                float dist = Vector3.Distance(other.position, transform.position);
+                print("Distance to other: " + dist);
+            }
+        }
+    }
+
+}
+
--- Assets/Class/_Vector3/Script/Vector3Set.cs
+++ Assets/Class/_Vector3/Script/Vector3Set.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vector3Set : MonoBehaviour
+namespace Class._Vector3.Script
 {
-    // Start is called before the first frame update
-    // Use this to set the new position of the GameObject
-    Vector3 m_MyPosition;
-
-    // Set an external Transform in the Inspector which is the GameObject’s starting point
-    public Transform m_NewTransform;
-
-    void Start()
+    public class Vector3Set : MonoBehaviour
     {
-        // Set the new Vector to be that of the Transform you attach in the Inspector
-        m_MyPosition.Set(m_NewTransform.position.x, m_NewTransform.position.y, 0);
-    }
+        // Start is called before the first frame update
+        // Use this to set the new position of the GameObject
+        Vector3 m_MyPosition;
 
-    void OnGUI()
-    {
-        // Press the Button to set the GameObject to this new position
-        if (GUI.Button(new Rect(0, 0, 100, 40), "Set"))
+        // Set an external Transform in the Inspector which is the GameObject’s starting point
+        public Transform m_NewTransform;
+
+        void Start()
         {
-            transform.position = m_MyPosition;
+            // Set the new Vector to be that of the Transform you attach in the Inspector
+            m_MyPosition.Set(m_NewTransform.position.x, m_NewTransform.position.y, 0);
+        }
+
+        void OnGUI()
+        {
+            // Press the Button to set the GameObject to this new position
+            if (GUI.Button(new Rect(0, 0, 100, 40), "Set"))
+            {
+                transform.position = m_MyPosition;
+            }
         }
     }
 }
+
+
