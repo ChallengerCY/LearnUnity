9f5ca78bdcc11a3be74e84e04e27741c8b11553d
ChallengerCY <790597004@qq.com>
2020-02-26 22:28:07 +0800

    整理vector3 整理vector3 Constructor 官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3Constructor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3Constructor : MonoBehaviour
+    {
+        Vector3 myVector;
+        Rigidbody m_Rigidbody;
+        float m_Speed = 2.0f;
+
+        void Start()
+        {
+            //Set the vector, which you use to move the RigidBody upwards straight away
+            myVector = new Vector3(0.0f, 1.0f, 0.0f);
+            //Fetch the RigidBody you attach to the GameObject
+            m_Rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            //Move the RigidBody upwards at the speed you define
+            m_Rigidbody.velocity = myVector * m_Speed;
+        }
+    }
+}
+
+
