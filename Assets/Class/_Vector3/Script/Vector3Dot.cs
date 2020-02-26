ad0a8049cf21eb5df8e57aa59924f27b61762cde
ChallengerCY <790597004@qq.com>
2020-02-26 23:30:50 +0800

    查看vector3 dot 官方的例子

---
+++ Assets/Class/_Vector3/Script/Vector3Dot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Vector3.Script
+{
+    public class Vector3Dot : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Transform other;
+
+        private void Start()
+        {
+            Debug.LogError(@"点积是一个浮点值，它等于将两个向量的大小相乘，然后乘以向量之间角度的余弦值。
+                                对于 normalized 向量，如果它们指向完全相同的方向，Dot 返回 1；
+                                如果它们指向完全相反的方向，返回 -1；如果向量彼此垂直，则 Dot 返回 0。");
+        }
+
+        void Update()
+        {
+            if (other)
+            {
+                Vector3 forward = transform.TransformDirection(Vector3.forward);
+                Vector3 toOther = other.position - transform.position;
+
+                if (Vector3.Dot(forward, toOther) < 0)
+                {
+                    print("The other transform is behind me!");
+                }
+            }
+        }
+    }
+}
+
+
