7a22805f3618fab609d3f85476dd52797b4116a0
ChallengerCY <790597004@qq.com>
2020-02-29 10:13:40 +0800

    查看vector3  官方的例子

---
+++ Assets/Class/_BoxCollider/Script/BoxColliderCenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._BoxCollider.Script
+{
+    public class BoxColliderCenter : MonoBehaviour
+    {
+        public BoxCollider BoxCollider;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.LogError(BoxCollider.center);
+        }
+
+    }
+}
---
+++ Assets/Class/_Ray/Script/RayConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Class._Ray.Script
+{
+    public class RayConstructor : MonoBehaviour
+    {
+        void Start()
+        {
+            // Create a ray from the transform position along the transform's z-axis
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            Debug.LogError($"射线发出后10m的点是{ray.GetPoint(10)}");
+
+            Debug.LogError($"描述当前射{ray.ToString()}");
+
+            Debug.LogError($"射线的方向是{ray.direction}");
+
+            Debug.LogError($"射线的起点是{ray.origin}");
+        }
+    }
+}
