abbd43b7b83c30ff2fbb96469ffe5fe49afc05ca
ChallengerCY <790597004@qq.com>
2020-03-01 23:27:51 +0800

    查看vector3  官方的例子

--- Assets/Class/_Collider/Script/ColliderOnCollision.cs
+++ Assets/Class/_Collider/Script/ColliderOnCollision.cs
@@ -9,17 +9,23 @@ namespace Class._Collider.Script
     {
         private void OnCollisionEnter(Collision other)
         {
-
+            foreach (ContactPoint contact in other.contacts)
+            {
+                Debug.DrawRay(contact.point, contact.normal, Color.white);
+            }
         }
 
         private void OnCollisionStay(Collision other)
         {
-
+            foreach (ContactPoint contact in other.contacts)
+            {
+                Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
+            }
         }
 
         private void OnCollisionExit(Collision other)
         {
-
+            print("No longer in contact with " + other.transform.name);
         }
     }
 }
---
+++ Assets/Class/_ContactPoint/Script/ContactPointProperties.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactPointProperties : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        print("Points colliding: " + other.contacts.Length);
+        print("First normal of the point that collide: " + other.contacts[0].normal);
+        print("First point that collided: " + other.contacts[0].point);
+        
+        print("分离距离是" + other.contacts[0].separation);
+        Debug.LogError(other.gameObject.name);
+        Debug.LogError(other.contacts[0].otherCollider.name);
+        Debug.LogError(other.contacts[0].thisCollider.name);
+    }
+}
